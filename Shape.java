30aca38d64dda5f940d08c9a19cb8f6fdb7491b1
Lara <45492216+larahenney@users.noreply.github.com>
2018-12-13 14:45:40 -0600

    Delete Shape.java

--- Shape.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.shapes;
-
-public abstract class Shape {
-	
-	public abstract double calculateArea(); //abstract keyword used to create abstract component 
-	// here is an abstract method that has to be used with each shape made 
-	
-
-}
